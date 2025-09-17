4c0c94a64ef132fd4fe5ec46c3fab170d298a0da
Tbawi <tbawi@ftstudent.org>
2025-09-17 11:47:27 -0400

    commit changes

--- script/Character.cs
+++ script/Character.cs
@@ -5,37 +5,37 @@ using System;
 
 public interface ICharacter
 {
-    void _PhysicsProcess(global::System.Double delta);
-    void _Ready();
+	void _PhysicsProcess(global::System.Double delta);
+	void _Ready();
 }
 
 
 public partial class Character : CharacterBody2D, ICharacter
 {
-    private int speed = 500;
-    private int jump_height = 5;
+	private int speed = 500;
+	private int jump_height = 5;
 
-    public override void _Ready() { }
+	public override void _Ready() { }
 
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (Input.IsActionPressed("right"))
-        {
-            Position += new Vector2(speed * (float)delta, 0);
-        }
-        if (Input.IsActionPressed("left"))
-        {
-            Position -= new Vector2(speed * (float)delta, 0);
-        }
-        if (Input.IsActionPressed("jump"))
-        {
-            Velocity += new Vector2(0, jump_height);
-        }
-        if (!this.IsOnFloor())
-        {
-            Velocity -= GetGravity();
-        }
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		if (Input.IsActionPressed("right"))
+		{
+			Position += new Vector2(speed * (float)delta, 0);
+		}
+		if (Input.IsActionPressed("left"))
+		{
+			Position -= new Vector2(speed * (float)delta, 0);
+		}
+		if (Input.IsActionPressed("jump"))
+		{
+			Velocity += new Vector2(0, jump_height);
+		}
+		if (!this.IsOnFloor())
+		{
+			Velocity -= GetGravity();
+		}
+	}
 
 }
