339391eae6dc01e37d398d6ffd7509e78daa8a5a
Isaiah-hub-org <ipierre0@ftstudent.org>
2025-09-17 12:12:18 -0400

    h

--- script/Character.cs
+++ script/Character.cs
@@ -5,13 +5,19 @@ using System;
 
 public interface ICharacter
 {
+<<<<<<< Updated upstream
 	void _PhysicsProcess(global::System.Double delta);
 	void _Ready();
+=======
+    void _PhysicsProcess(global::System.Double delta);
+    void _Ready();
+>>>>>>> Stashed changes
 }
 
 
 public partial class Character : CharacterBody2D, ICharacter
 {
+<<<<<<< Updated upstream
 	private int speed = 500;
 	private int jump_height = 5;
 
@@ -37,5 +43,32 @@ public partial class Character : CharacterBody2D, ICharacter
 			Velocity -= GetGravity();
 		}
 	}
+=======
+    private int speed = 500;
+    private int jump_height = 5;
+
+    public override void _Ready() { }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (Input.IsActionPressed("right"))
+        {
+            Position += new Vector2(speed * (float)delta, 0);
+        }
+        if (Input.IsActionPressed("left"))
+        {
+            Position -= new Vector2(speed * (float)delta, 0);
+        }
+        if (Input.IsActionPressed("jump"))
+        {
+            Velocity += new Vector2(0, jump_height);
+        }
+        if (!this.IsOnFloor())
+        {
+            Velocity -= GetGravity();
+        }
+    }
+>>>>>>> Stashed changes
 
 }
